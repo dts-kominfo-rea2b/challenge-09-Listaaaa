49a2e066c724acfb3ec67839b1cf79e417b969e3
Listaaaa <aisyah12hidayat@gmail.com>
2022-06-26 08:51:44 +0800

    update

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,10 +2,21 @@
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 import React from 'react';
 
-const Contact = () => {
-    return (
-        <></>
+const Contact = ({data}) => {
+    const {name, phone, email, photo} = data
+    return(
+        <div className='Boxes'>
+            <div>
+                <img src={photo} alt='BrokenPicture' className='PhotoProfile'/>
+            </div>
+            <div className='DataContact'>
+                <h2 className='name'>{name}</h2>
+                <h3 className='phone'>{phone}</h3>
+                <h3 className='email'>{email}</h3>
+            </div>
+        </div>
+    
     )
-}
+} 
 
 export default Contact;
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react';
 
 const Header = () => {
     return (
-        <></>
+        <div>
+            <h1 className='Title'>Call a Friend</h1>
+            <h4 className='SubTitle'>your friendly contact app</h4>
+        </div>
     )
 }
 
